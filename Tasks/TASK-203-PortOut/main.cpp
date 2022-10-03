9f519681447c9d6ddeeac0c87b8d87a4e420ae60
Samuel Southgate <samuel.bertrand-southgate@students.plymouth.ac.uk>
2022-10-03 14:04:28 +0100

    Lab Practicals 202-205. 03/10/2022
    Achieved all tasks for labs 202 to 205.
    Harry didn't do on his account send email with code

--- Tasks/TASK-203-PortOut/main.cpp
+++ Tasks/TASK-203-PortOut/main.cpp
@@ -1,21 +1,30 @@
 #include "mbed.h"
 
 // Hardware Definitions
-#define TRAF_GRN1_PIN PC_6
-#define TRAF_YEL1_PIN PC_3
-#define TRAF_RED1_PIN PC_2
-#define LEDMASK 0b0000000001001100
+#define TRAF_GRN1_PIN   PC_6
+#define TRAF_YEL1_PIN   PC_3
+#define TRAF_RED1_PIN   PC_2
+#define OBLED_0         PB_0
+#define OBLED_1         PB_7
+#define OBLED_2         PB_14
+#define LEDMASK     0b0000000001001100
+#define LEDMASK2    0b0100000010000001
 // Objects
 //BusOut leds(TRAF_RED1_PIN, TRAF_YEL1_PIN, TRAF_GRN1_PIN);
 PortOut leds(PortC, LEDMASK);
+PortOut OBleds(PortB, LEDMASK2);
 
 int main()
 {
     while (true) {
-        leds = 0;   //Binary 000
+        leds = leds ^ 0xFFFF;
+        OBleds = OBleds ^ 0xFFFF;
+        //leds = 0;
+        //OBleds = 0;   //Binary 000
         wait_us(500000);
-        leds = 0xFF;   //Binary 11111111
-        wait_us(500000);    
+        //leds = 0xFF;   //Binary 11111111
+        //OBleds = 0xFFFF;
+        //wait_us(500000);  
     }
 }
 
--- Tasks/Task-202-BusOut/main.cpp
+++ Tasks/Task-202-BusOut/main.cpp
@@ -4,19 +4,23 @@
 #define TRAF_GRN1_PIN PC_6
 #define TRAF_YEL1_PIN PC_3
 #define TRAF_RED1_PIN PC_2
+#define Extra_LED_1     PB_0
+#define Extra_LED_2     PB_7
+#define Extra_LED_3     PB_14     
 
 // Objects
 // DigitalOut grn(TRAF_GRN1_PIN);
 // DigitalOut yel(TRAF_YEL1_PIN);
 // DigitalOut red(TRAF_RED1_PIN,1);
-BusOut leds(TRAF_RED1_PIN, TRAF_YEL1_PIN, TRAF_GRN1_PIN);
+BusOut leds(TRAF_RED1_PIN, TRAF_YEL1_PIN, TRAF_GRN1_PIN, Extra_LED_1, Extra_LED_2, Extra_LED_3);
 
 int main()
 {
     while (true) {
         leds = 0;   //Binary 000
         wait_us(500000);
-        leds = 7;   //Binary 111
+
+        leds = 63;   //Binary 111
         wait_us(500000);    
     }
 }
